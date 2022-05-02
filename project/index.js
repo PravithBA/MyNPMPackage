a195190b80210dfc8c2a78c5dc80527b500abd5c
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-03 04:09:16 +0530

    Added files

---
+++ project/index.js
@@ -0,0 +1,28 @@
+function has(array,str){
+    if(!array || !str )
+        return console.error('Didnt pass arguments')
+    if(typeof array != 'array')
+        array = [array]
+    return (array.some(word => {return word.includes(str)}))
+}
+function dateTimeDifference(date1,date2,format){
+    if(new Date(date1) < new Date(date2)){
+        let temp = new Date(date1)
+        date1 = new Date(date2)
+        date2 = temp
+    }
+    else{
+        date1 = new Date(date1)
+        date2 = new Date(date2)
+    }
+    switch(format){
+        case 'milliseconds':return (date1-date2)/1000
+        case 'seconds':return (date1-date2)/(1000*60)
+        case 'minutes':return (date1-date2)/(1000 * 60 * 60)
+        case 'days':return (date1-date2)/(1000 * 60 * 60 * 24)
+        case 'months':return (date1.getMonth()-date2.getMonth())+((date1.getYear()-date2.getYear())*12)
+        case 'years':return date1.getYear()-date2.getYear()
+        default:return 'Invalid Format'
+    }
+}
+module.exports = {has,dateTimeDifference}
