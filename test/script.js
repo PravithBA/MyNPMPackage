d7a64dde960c7f4a514c6025e12aeda0e3e6230a
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-05 16:53:15 +0530

    lol

--- test/script.js
+++ test/script.js
@@ -1,23 +1,8 @@
-const lol = require('lol')
-console.log(lol.dateTimeDifference('01 Jan 2050 00:00:00 GMT','01 Jan 1990 00:00:00 GMT','days'))
+const {reWriteJson} = require("smpljavascript")
 
+let newData = {
+    "Mat": "40"
+}
+const jsonfile = ("./test.json")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reWriteJson(jsonfile,newData)
